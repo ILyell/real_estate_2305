62176b2744436c892acecb007436472f7ca9c96a
Ian Lyell <ian.lyell@yahoo.com>
2023-05-18 10:35:37 -0500

    Pass iteration 1 test

--- lib/room.rb
+++ lib/room.rb
@@ -2,13 +2,22 @@ class Room
 
   attr_reader :category, :length, :width
 
-  def initialize(category, length, width)
+  def initialize(category, length, width, painted = false)
     @category = category
     @length = length
     @width = width
+    @painted = painted
   end
 
   def area
     length * width.to_i
+  end
+
+  def is_painted?
+    @painted
+  end
+
+  def paint
+    @painted = true
   end
 end
