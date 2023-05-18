a8cfd616712f582b5f71d9e676f9cd9af1888a49
Ian Lyell <ian.lyell@yahoo.com>
2023-05-18 10:33:43 -0500

    Add test for iteration 1

--- spec/room_spec.rb
+++ spec/room_spec.rb
@@ -20,5 +20,27 @@ RSpec.describe Room do
       expect(room1.area).to eq(130)
       expect(room2.area).to eq(180)
     end
+
+    it 'is not painted by default' do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+
+      expect(room1.is_painted?).to eq(false)
+      expect(room2.is_painted?).to eq(false)
+    end
+
+    it 'can be painted' do
+      room1 = Room.new(:bedroom, 10, '13')
+      room2 = Room.new(:living_room, 15, '12')
+
+      expect(room1.is_painted?).to eq(false)
+      expect(room2.is_painted?).to eq(false)
+
+      room1.paint
+      expect(room1.is_painted?).to eq(true)
+      expect(room2.is_painted?).to eq(false)
+      room2.paint
+      expect(room2.is_painted?).to eq(true)
+    end
   end
 end
